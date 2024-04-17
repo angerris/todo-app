e824202947791d1c1e90f004699355b87381c1c2
arine <ariangerris@gmail.com>
2024-04-18 02:47:06 +0400

    modified ui + added tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,49 @@
+/* eslint-disable testing-library/no-node-access */
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
+import "./setupDomTests";
 
-test("renders learn react link", () => {
+test("adding a new todo", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const input = screen.getByPlaceholderText(/don't be shy, add some tasks!/i);
+  const addButton = screen.getByRole("button", { name: /add/i });
+  fireEvent.change(input, { target: { value: "Test new todo" } });
+  fireEvent.click(addButton);
+  expect(screen.getByText("Test new todo")).toBeInTheDocument();
+});
+
+test("deleting a todo", () => {
+  render(<App />);
+  const deleteButton = screen.getAllByRole("button", { name: /delete/i })[0];
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText("Todo text to delete")).not.toBeInTheDocument();
+});
+
+test("todo can be marked as done", () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText(/don't be shy, add some tasks!/i);
+  const addButton = screen.getByRole("button", { name: /add/i });
+  fireEvent.change(input, { target: { value: "Test new todo" } });
+  fireEvent.click(addButton);
+  const checkbox = screen.getByRole("checkbox");
+  fireEvent.click(checkbox);
+  expect(checkbox).toBeChecked();
+  fireEvent.click(checkbox);
+  expect(checkbox).not.toBeChecked();
+});
+
+test("editing a todo", () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText(/don't be shy, add some tasks!/i);
+  const addButton = screen.getByRole("button", { name: /add/i });
+  fireEvent.change(input, { target: { value: "current" } });
+  fireEvent.click(addButton);
+  const editButton = screen.getAllByRole("button", { name: /edit/i })[0];
+  fireEvent.click(editButton);
+  const editInput: any = screen.getByDisplayValue("current");
+  fireEvent.change(editInput, { target: { value: "updated" } });
+  const ok = screen.getByRole("button", { name: /ok/i });
+  fireEvent.click(ok);
+  expect(screen.queryByText("current")).not.toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Button, Input, List, Checkbox, Space } from "antd";
 import { EditOutlined, DeleteOutlined, PlusOutlined } from "@ant-design/icons";
 import styles from "./App.module.scss";
@@ -13,6 +13,7 @@ const App: React.FC = () => {
   const [inputText, setInputText] = useState<string>("");
   const [editingId, setEditingId] = useState<number | null>(null);
   const [editedText, setEditedText] = useState<string>("");
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     const storedTodos = localStorage.getItem("todos");
@@ -69,37 +70,49 @@ const App: React.FC = () => {
     setTodos(sortedTodos);
   };
 
+  const handleLoad = () => {
+    setIsLoaded(true);
+  };
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.todoWrapper}>
-        <div className={styles.topWrapper}>
-          {" "}
-          <h1 className={styles.title}>todos</h1>
-          <div className={styles.inputWrapper}>
-            <Input
-              placeholder="enter todo item"
-              value={inputText}
-              onChange={(e) => setInputText(e.target.value)}
-              onPressEnter={handleAddTodo}
-            />
-            <Button
-              type="primary"
-              icon={<PlusOutlined />}
-              onClick={handleAddTodo}
-            >
-              add
-            </Button>
+    <Suspense fallback="loading...">
+      <div className={styles.wrapper}>
+        <div className={styles.todoWrapper}>
+          <div className={styles.topWrapper}>
+            {" "}
+            <h1 className={styles.title}>todos</h1>
+            <div className={styles.inputWrapper}>
+              <iframe
+                src="https://lottie.host/embed/e4563179-3155-4efd-95df-5efb3cc7d022/EsrH6714Q1.json"
+                width={140}
+                height={150}
+                className={styles.animation}
+                style={{ display: isLoaded ? "block" : "none" }}
+                onLoad={handleLoad}
+              ></iframe>
+              <Input
+                placeholder="don't be shy, add some tasks! 🦈"
+                value={inputText}
+                onChange={(e) => setInputText(e.target.value)}
+                onPressEnter={handleAddTodo}
+              />
+              <Button
+                type="primary"
+                icon={<PlusOutlined />}
+                onClick={handleAddTodo}
+              >
+                add
+              </Button>
+            </div>
           </div>
-        </div>
-        {todos.length > 0 && (
-          <List
-            className={styles.taskWrapper}
-            dataSource={todos}
-            renderItem={(todo) => (
-              <List.Item
-                actions={[
-                  <Space>
-                    {editingId !== todo.id ? (
+          {todos.length > 0 && (
+            <List
+              className={styles.taskWrapper}
+              dataSource={todos}
+              renderItem={(todo) => (
+                <List.Item className={styles.taskItem}>
+                  {editingId !== todo.id ? (
+                    <div className={styles.actions}>
                       <Button
                         type="link"
                         icon={<EditOutlined />}
@@ -108,49 +121,52 @@ const App: React.FC = () => {
                           setEditedText(todo.text);
                         }}
                       />
-                    ) : (
                       <Button
-                        type="primary"
-                        onClick={() => handleEditTodo(todo.id, editedText)}
-                      >
-                        OK
-                      </Button>
-                    )}
+                        type="link"
+                        danger
+                        icon={<DeleteOutlined />}
+                        onClick={() => handleDeleteTodo(todo.id)}
+                      />
+                      <Checkbox
+                        type="checkbox"
+                        name="checkbox"
+                        checked={todo.done}
+                        onChange={() => handleToggleDone(todo.id)}
+                      />
+                    </div>
+                  ) : (
                     <Button
-                      type="link"
-                      danger
-                      icon={<DeleteOutlined />}
-                      onClick={() => handleDeleteTodo(todo.id)}
+                      type="primary"
+                      onClick={() => handleEditTodo(todo.id, editedText)}
+                    >
+                      OK
+                    </Button>
+                  )}
+
+                  {todo.id === editingId ? (
+                    <Input
+                      style={{ maxWidth: "70%" }}
+                      value={todo.text}
+                      autoFocus
+                      onChange={(e) => handleEditTodo(todo.id, e.target.value)}
+                      onPressEnter={() => setEditingId(0)}
+                      onBlur={() => setEditingId(0)}
                     />
-                    <Checkbox
-                      checked={todo.done}
-                      onChange={() => handleToggleDone(todo.id)}
-                    />
-                  </Space>
-                ]}
-              >
-                {todo.id === editingId ? (
-                  <Input
-                    value={todo.text}
-                    autoFocus
-                    onChange={(e) => handleEditTodo(todo.id, e.target.value)}
-                    onPressEnter={() => setEditingId(0)}
-                    onBlur={() => setEditingId(0)}
-                  />
-                ) : todo.done ? (
-                  <p className={styles.todoText}>
-                    {" "}
-                    <del>{todo.text} </del>
-                  </p>
-                ) : (
-                  <p className={styles.todoText}> {todo.text}</p>
-                )}
-              </List.Item>
-            )}
-          />
-        )}
+                  ) : todo.done ? (
+                    <p className={styles.todoText}>
+                      {" "}
+                      <del>{todo.text} </del>
+                    </p>
+                  ) : (
+                    <p className={styles.todoText}> {todo.text}</p>
+                  )}
+                </List.Item>
+              )}
+            />
+          )}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
